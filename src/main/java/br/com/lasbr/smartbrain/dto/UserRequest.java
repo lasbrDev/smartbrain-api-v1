58bc031681118b0da9b168330d6ed8b4ca8af490
Luciano Alves <lasbr_alves@outlook.com>
2023-11-29 14:10:31 -0300

    feat: definitions in dtos -> validations and responses when registering users.

--- src/main/java/br/com/lasbr/smartbrain/dto/UserRequest.java
+++ src/main/java/br/com/lasbr/smartbrain/dto/UserRequest.java
@@ -1,7 +1,17 @@
 package br.com.lasbr.smartbrain.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
     public record UserRequest(
+
+            @NotBlank(message = "Nome não pode estar em branco.")
             String name,
+            @NotBlank(message = "Email não pode estar em branco.")
+            @Email(message = "Email inválido")
             String email,
+            @NotBlank(message = "Senha não pode estar em branco.")
+            @Size(min = 8, message = "A senha deve ter pelo menos 8 caracteres.")
             String password) {
     }
--- src/main/java/br/com/lasbr/smartbrain/dto/UserResponse.java
+++ src/main/java/br/com/lasbr/smartbrain/dto/UserResponse.java
@@ -1,4 +1,10 @@
 package br.com.lasbr.smartbrain.dto;
 
-    public record UserResponse(String name, String email) {
+import br.com.lasbr.smartbrain.model.User;
+
+    public record UserResponse(Long id, String name, String email) {
+
+        public UserResponse(User user) {
+            this(user.getId(), user.getName(), user.getEmail());
+        }
     }
