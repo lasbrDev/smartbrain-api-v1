222bd4a23e66f03cfd951b185325cc1929f6833b
Luciano Alves <lasbr_alves@outlook.com>
2023-11-29 00:24:27 -0300

    feat: added a new project java spring boot to start migration.

---
+++ src/main/java/br/com/lasbr/smartbrain/SmartBrainApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lasbr.smartbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBrainApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBrainApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lasbr/smartbrain/SmartBrainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lasbr.smartbrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBrainApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
